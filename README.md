07df7879096f2aa739d02af5c791aaabe7b3bfee
SnapFlip20 <snapflip20@gmail.com>
2023-01-14 15:41:09 +0900

    20230114

--- README.md
+++ README.md
@@ -1,1 +1,151 @@
 # Geometry
+## Point
+* 새로운 point 객체 선언
+```python
+a = point(1)
+b = point(1, 2)
+c = point(1, 2, 3)
+print(a, b, c)
+
+# >>> point(1, 0, 0) point(1, 2, 0) point(1, 2, 3)
+```
+```python
+a = point((1))
+b = point([1, 2])
+c = point(point(1, 2, 3))
+print(a, b, c)
+
+# >>> point(1, 0, 0) point(1, 2, 0) point(1, 2, 3)
+```
+* 수치 연산
+    * 두 점 사이의 거리
+    ```python
+    point.dist(a, b) # 두 점 사이의 유클리드 거리
+    point.dist2(a, b) # 두 점 사이의 유클리드 거리 제곱
+    point.dist_taxi(a, b) # 두 점 사이의 택시 거리
+    ```
+    * 대칭 이동
+    ```python
+    point.flipx(a) # x축에 대하여 대칭 이동
+    point.flipy(a) # y축에 대하여 대칭 이동
+    point.flipz(a) # z축에 대하여 대칭 이동
+    point.flipxy(a) # xy평면에 대하여 대칭 이동
+    point.flipxz(a) # xz평면에 대하여 대칭 이동
+    point.flipyz(a) # yz평면에 대하여 대칭 이동
+    point.flipxyz(a) # point(0, 0, 0)에 대하여 대칭 이동(= -point(a))
+    ```
+    * 평행 이동
+    ```python
+    a.move(1) # x축으로 1만큼 평행 이동 
+    a.move(1, 1) # x, y축으로 1만큼 평행 이동 
+    a.move(1, 1, 1) # x, y, z축으로 1만큼 평행 이동 
+    ```
+    * 두 점 사이의 기울기
+    ```python
+    point.slope(a, b) # y축에 평행한 경우 inf 반환
+    ```
+    * 특정 좌표 기준 거리가 가까운 순으로 정렬
+    ```python
+    point.sort_closest(point_lst, main_point)
+    ```
+    * 특정 좌표 기준 반시계방향으로 정렬
+    ```python
+    point.sort_acw(point_lst, main_point)
+    ```
+  
+## Vector
+* 새로운 vector 객체 선언
+```python
+a = vector(1)
+b = vector(1, 2)
+c = vector(1, 2, 3)
+print(a, b, c)
+
+# >>> vector(1, 0, 0) vector(1, 2, 0) vector(1, 2, 3)
+```
+```python
+a = vector((1))
+b = vector([1, 2])
+c = vector(vector(1, 2, 3))
+print(a, b, c)
+
+# >>> vector(1, 0, 0) vector(1, 2, 0) vector(1, 2, 3)
+```
+* 수치 연산
+    * 벡터합
+    ```python
+    a = vector(1, 2, 3)
+    b = vector(2, -3, 1)
+    print(a+b, a-b)
+
+    # >>> vector(3, -1, 4) vector(-1, 5, 2)
+    ```
+    * 스칼라곱
+    ```python
+    a = vector(1, 2, 3)
+    print(a*3, 2*a)
+    print(a/3)
+
+    # >>> vector(3, 6, 9) vector(2, 4, 6)
+    # >>> vector(0.3333333333333333, 0.6666666666666666, 1.0)
+    ```
+    * 절댓값(벡터의 크기)
+    ```python
+    a = vector(1, 2, 3)
+    print(abs(a), a.size())
+
+    # >>> 3.7416573867739413 3.7416573867739413
+    ```
+    * 두 벡터가 이루는 각(단위: degree)
+    ```python
+    a = vector(1, 2, 3)
+    b = vector(-1, -3, 2)
+    print(vector.angle(a, b))
+
+    # >>> 94.09604375815233
+    ```
+    * 두 벡터가 이루는 삼각형의 넓이
+    ```python
+    a = vector(1, 2, 3)
+    b = vector(-1, -3, 2)
+    print(vector.area3(a, b))
+
+    # >>> 1.0580798500470525
+    ```
+    * 두 벡터가 이루는 평행사변형의 넓이
+    ```python
+    a = vector(1, 2, 3)
+    b = vector(-1, -3, 2)
+    print(vector.area4(a, b))
+
+    # >>> 2.116159700094105
+    ```
+    * 두 벡터의 외적
+    ```python
+    a = vector(1, 2, 3)
+    b = vector(-1, -3, 2)
+    print(vector.cross(a, b))
+
+    # >>> vector(13, 0, -1)
+    ```
+    * 두 벡터의 내적
+    ```python
+    a = vector(1, 2, 3)
+    b = vector(-1, -3, 2)
+    print(vector.dot(a, b))
+
+    # >>> -1
+    ```
+    * 역벡터
+    ```python
+    a = vector(-2, 1, 3)
+    print(a.reverse())
+
+    # >>> vector(2, -1, -3)
+    ```
+    * 영벡터
+    ```python
+    print(vector.zero())
+
+    # >>> vector(0, 0, 0)
+    ```
---
+++ geometry.py
@@ -0,0 +1,268 @@
+#-*- coding:utf-8 -*-
+
+from math import acos, degrees, fabs, sin, sqrt
+
+class point:
+    def __init__(self, x=0, y=0, z=0):
+        if isinstance(x, (list, tuple, point)):
+            if len(x) == 1:
+                _cv = [x[0], 0, 0]
+                x, y, z = _cv
+            elif len(x) == 2:
+                _cv = [x[0], x[1], 0]
+                x, y, z = _cv
+            elif len(x) == 3:
+                _cv = [x[0], x[1], x[2]]
+                x, y, z = _cv
+        self.v = [x, y, z]
+        self.x = x
+        self.y = y
+        self.z = z
+
+    @classmethod
+    def invert(cls, v):
+        if not isinstance(v, list):
+            raise TypeError
+        cv = cls()
+        cv.v = v
+        return cv
+
+    def __repr__(self):
+        comp = ', '.join([repr(x) for x in self.v])
+        return f'{type(self).__name__}({comp})'
+    
+    def __str__(self):
+        comp = ', '.join([str(x) for x in self.v])
+        return f'point({comp})'
+
+    def __pos__(self):
+        return self
+
+    def __neg__(self):
+        return point(-self.x, -self.y, -self.z)
+    
+    def __eq__(self, other):
+        if not isinstance(other, point):
+            raise TypeError
+        return self.v == other.v
+
+    def __ne__(self, other):
+        if not isinstance(other, point):
+            raise TypeError
+        return self.v != other.v
+
+    def __len__(self):
+        return len(self.v)
+    
+    def __getitem__(self, i):
+        if i > 2:
+            raise IndexError
+        return self.v[i]
+
+    def area(lst):
+        ...
+
+    def ccw(a, b, c):
+        a_b = vector.to_vector(a, b)
+        b_c = vector.to_vector(b, c)
+        return vector.cross(a_b, b_c)[2]
+
+    def dist(a, b):
+        return sqrt((a.x-b.x)**2 + (a.y-b.y)**2 + (a.z-b.z)**2)
+
+    def dist2(a, b):
+        return (a.x-b.x)**2 + (a.y-b.y)**2 + (a.z-b.z)**2
+
+    def dist_taxi(a, b):
+        return fabs(a.x-b.x) + fabs(a.y-b.y) + fabs(a.z-b.z)
+
+    def flipx(self):
+        return point(-self.x, self.y, self.z)
+
+    def flipy(self):
+        return point(self.x, -self.y, self.z)
+
+    def flipz(self):
+        return point(self.x, self.y, -self.z)
+
+    def flipxy(self):
+        return point(-self.x, -self.y, self.z)
+
+    def flipxz(self):
+        return point(-self.x, self.y, -self.z)
+
+    def flipyz(self):
+        return point(self.x, -self.y, -self.z)
+
+    def flipxyz(self):
+        return point(-self.x, -self.y, -self.z)
+
+    def move(self, value_x=0, value_y=0, value_z=0):
+        return point(self.x+value_x, self.y+value_y, self.z+value_z)
+    
+    def slope(a, b):
+        if a.x != b.x:
+            return 1.0*(a.y-b.y)/(a.x-b.x)
+        else:
+            return float('inf')
+
+    def sort_closest(lst, pt):
+        _s = []
+        while pt in lst:
+            _s.append(lst.pop(lst.index(pt)))
+        sorted_lst = sorted(lst, key=lambda _pt: point.dist(_pt, pt))
+        return _s+sorted_lst
+
+    def sort_acw(lst, pt):
+        _s = []
+        while pt in lst:
+            _s.append(lst.pop(lst.index(pt)))
+        sorted_lst = sorted(lst, key=lambda _pt: (point.slope(_pt, pt), point.dist(_pt, pt)))
+        return _s+sorted_lst
+
+class vector:
+    def __init__(self, x=0, y=0, z=0):
+        if isinstance(x, (list, tuple, vector)):
+            if len(x) == 1:
+                _cv = [x[0], 0, 0]
+                x, y, z = _cv
+            elif len(x) == 2:
+                _cv = [x[0], x[1], 0]
+                x, y, z = _cv
+            elif len(x) == 3:
+                _cv = [x[0], x[1], x[2]]
+                x, y, z = _cv
+        self.v = [x, y, z]
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    @classmethod
+    def invert(cls, v):
+        if not isinstance(v, list):
+            raise TypeError
+        cv = cls()
+        cv.v = v
+        return cv
+
+    def __repr__(self):
+        comp = ', '.join([repr(x) for x in self.v])
+        return f'{type(self).__name__}({comp})'
+    
+    def __str__(self):
+        comp = ', '.join([str(x) for x in self.v])
+        return f'vector({comp})'
+    
+    def __add__(self, other):
+        if not isinstance(other, vector):
+            if other == 0:
+                return self
+            raise TypeError
+        _add = [a+b for (a, b) in zip(self.v, other.v)]
+        return vector.invert(_add)
+
+    def __radd__(self, other):
+        if not isinstance(other, vector):
+            if other == 0:
+                return self
+            raise TypeError
+        _radd = [a+b for (a, b) in zip(self.v, other.v)]
+        return vector.invert(_radd)
+
+    def __sub__(self, other):
+        if not isinstance(other, vector):
+            raise TypeError
+        _sub = [a-b for (a, b) in zip(self.v, other.v)]
+        return vector.invert(_sub)
+
+    def __mul__(self, scalar):
+        if not isinstance(scalar, (int or float)):
+            raise TypeError
+        _mul = [x*scalar for x in self.v]
+        return vector.invert(_mul)
+
+    def __rmul__(self, scalar):
+        if not isinstance(scalar, (int or float)):
+            raise TypeError
+        _mul = [x*scalar for x in self.v]
+        return vector.invert(_mul)
+
+    def __truediv__(self, scalar):
+        if not isinstance(scalar, (int or float)):
+            raise TypeError
+        _tdiv = [x/scalar for x in self.v]
+        return vector.invert(_tdiv)
+
+    def __floordiv__(self, scalar):
+        if not isinstance(scalar, (int or float)):
+            raise TypeError
+        _fdiv = [x//scalar for x in self.v]
+        return vector.invert(_fdiv)
+    
+    def __abs__(self):
+        return sum([x**2 for x in self.v])**0.5
+
+    def __pos__(self):
+        return self
+
+    def __neg__(self):
+        return vector(-self.x, -self.y, -self.z)
+
+    def __eq__(self, other):
+        if not isinstance(other, vector):
+            raise TypeError
+        return list(self) == list(other)
+
+    def __ne__(self, other):
+        if not isinstance(other, vector):
+            raise TypeError
+        return list(self) != list(other)
+
+    def __len__(self):
+        return len(self.v)
+    
+    def __getitem__(self, i):
+        if i > 2:
+            raise IndexError
+        return self.v[i]
+    
+    def angle(self, other):
+        if not isinstance(other, vector):
+            raise TypeError
+        return degrees(acos(vector.dot(self, other)/(abs(self)*abs(other))))
+
+    def area3(self, other):
+        if not isinstance(other, vector):
+            raise TypeError
+        return abs((abs(self)*abs(other)*sin(vector.angle(self, other)))/2)
+
+    def area4(self, other):
+        if not isinstance(other, vector):
+            raise TypeError
+        return abs(abs(self)*abs(other)*sin(vector.angle(self, other)))
+
+    def cross(v1, v2):
+        if not isinstance(v2, vector):
+            raise TypeError
+        _cross = [v1[1]*v2[2]-v1[2]*v2[1], v1[2]*v2[0]-v1[0]*v2[1], v1[0]*v2[1]-v1[1]*v2[0]]
+        return vector.invert(_cross)
+
+    def dot(self, other):
+        if not isinstance(other, vector):
+            raise TypeError
+        return sum(v_x*v_y for (v_x, v_y) in zip(self, other))
+
+    def reverse(self):
+        return vector(-self.x, -self.y, -self.z)
+
+    def size(self):
+        return sum([x**2 for x in self.v])**0.5
+
+    def to_vector(pt1=point, pt2=point): # pt1: from / pt2: to
+        _v = [v2-v1 for (v1, v2) in zip(pt1, pt2)]
+        return vector.invert(_v)
+
+    def zero():
+        return vector(0, 0, 0)
+
+
